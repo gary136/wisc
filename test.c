7cddae1f920bae0cbfd1872710d459e05f111c9f
Gary <sarcas0705@gmail.com>
2021-10-10 08:08:31 -0500

    second version

--- test.c
+++ test.c
@@ -10,7 +10,16 @@
 
 size_t MAXBUFR = 1024;
 char *prompt = "wish> ";
-char *syscalls[3] = {"ls", "pwd", "cd"}; 
+char* path[20];
+int pathsize;
+// char *path1 = "/bin/";
+// char *path2 = "/usr/bin/";
+char *builtins[3] = {"exit", "cd", "path"};     
+char error_message[30] = "An error has occurred\n";
+char buf[45];
+char buf1[45];
+char buf2[45];
+int isSyscall;
 
 struct command {
 	int argc;
@@ -18,15 +27,56 @@ struct command {
 	enum builtin_t {NONE, QUIT, JOBS, BG, FG} builtin;
 };
 
-int isSyscall (char* s) {
-	int len = sizeof(syscalls)/sizeof(syscalls[0]);
-	for(int i = 0; i < len; ++i){
-		if(!strcmp(syscalls[i], s)) return 0;
+// int isSyscall (char* s) {
+int translate(struct command *cmd) { 
+	// check if cmd->argv[0]) is a system call
+	// if not then print error message
+	// if yes concatenate it with the path
+
+	printf("checking %s syscall\n", cmd->argv[0]);
+
+	// try path1
+	// tempsyscall = syscall; 
+	// temppath = path1;
+	for (int i=0; i<pathsize; i++) {
+		strcpy(buf, "");
+		strcat(buf, path[i]);
+		strcat(buf, cmd->argv[0]);
+		printf("try %s\n", buf);
+		if (access(buf, X_OK)!=-1) { // path[i] work
+			cmd->argv[0] = buf;
+			return 1;
+		}
 	}
-	return 1;
+	// strcpy(buf1, "");
+	// strcat(buf1, path1);
+	// strcat(buf1, s);
+	// printf("try %s\n", buf1);
+	// if (access(buf1, X_OK)!=-1) { // path1 work
+	// 	// printf("c\n");
+	// 	return 1;
+	// } else {	// try path2		
+	// 	strcpy(buf2, "");	
+	// 	strcat(buf2, path2);
+	// 	strcat(buf2, s);
+	// 	printf("try %s\n", buf1);
+	// 	if (access(buf2, X_OK)!=-1) { // path2 work
+	// 		return 2;
+	// 	}
+	// }
+	printf("Not a system call\n");
+	return 0;
 }
 
-char* parse(const char *cmdline, struct command *cmd) {
+int findBuiltin (char* s) {
+	int len = sizeof(builtins)/sizeof(builtins[0]);
+	for(int i = 0; i < len; ++i){
+		if(strcmp(builtins[i], s)==0) return 1;
+	}
+	return 0;
+}
+
+void parse(const char *cmdline, struct command *cmd) {
 	// inilize
 	cmd->argc=0;
 	char *tok, *ret;
@@ -38,120 +88,122 @@ char* parse(const char *cmdline, struct command *cmd) {
 		tok = strsep(&cmdline, " ");
 		cmd->argv[cmd->argc] = tok;
 		cmd->argc++;
-		printf("tok: %s, cmdline: [%s], argc:%d\n", cmd->argv[cmd->argc-1], cmdline, cmd->argc);
+		// printf("tok: %s, cmdline: [%s], argc:%d\n", cmd->argv[cmd->argc-1], cmdline, cmd->argc);
 	}
 	// add last one
 	tok = cmdline;
 	cmdline = "";
 	cmd->argv[cmd->argc] = tok;
 	cmd->argc++;
-	printf("tok: %s, cmdline: [%s], argc:%d\n", tok, cmdline, cmd->argc);
+	// printf("tok: %s, cmdline: [%s], argc:%d\n", tok, cmdline, cmd->argc);
 
-	// cmd->argv[cmd->argc] = NULL; // mark the end
+	cmd->argv[cmd->argc] = NULL; // mark the end
 
-	// for (int i=0; i<cmd->argc; i++) {
-	// 	printf("%d, cmd: %s\n", i, cmd->argv[i]);
-	// }
-	return tok;
+	for (int i=0; i<cmd->argc; i++) {
+		printf("%d, cmd: %s\n", i, cmd->argv[i]);
+	}
+	// return tok;
 }
 
-// int parse(const char *cmdline, struct command *cmd){
-	// char array[MAXBUFR];
-	// const char delims[10] = " \t\r\\n";
-	// char *line = array;
-	// char *token, *endline;
-	// int is_bg;
 
-	// // if (cmdline==NULL)
 
-	// (void) strncpy(line, cmdline, MAXBUFR);
-	// endline = line + strlen(line);
-	// printf("line: %s, endline: %s\n", line, endline);
+// void translate(struct command *cmd) {
+// 	// if (isSyscall(syscall)==1){
+// 	// FILE *cmd=popen(syscall, "r");
+//     // fgets(res, sizeof(res), cmd);  
+//     // // printf("inside res: %s\n", res);
+//     // pclose(cmd);}
+// 	// char* syscall
 
-	// cmd->argc=0;
+// 	char* temppath;
+// 	int rc;
 
-	// while (line<endline) {
-	// 	line +=strspn(line, delims);
-	// 	if (line>=endline) break;
+// 	rc = isSyscall(cmd->argv[0]);	
+// 	printf("rc = %d\n", rc);
+// 	if (rc==0) {
+// 		printf("Not a system call\n");
+// 	} else if (rc==1) {
+// 		strcpy(buf1, "");
+// 		strcat(buf1, path1);
+// 		strcat(buf1, cmd->argv[0]);
+// 		cmd->argv[0] = buf1;
+// 	} else if (rc==2) {
+// 		strcpy(buf2, "");
+// 		strcat(buf2, path2);
+// 		strcat(buf2, cmd->argv[0]);
+// 		cmd->argv[0] = buf2;
+// 	}
 
-	// 	token = line + strcspn(line, delims);
-
-	// 	*token = '\0';
-
-	// 	cmd->argv[cmd->argc++] = line;
-		
-	// 	if (cmd->argc >= MAXARGS-1) break;
-	// 	line = token + 1;
-	// }
-
-	// cmd->argv[cmd->argc] = NULL;
-
-	// if (cmd->argc==0) return 1;
-
-	// // cmd->builtin = parseBuiltin(cmd);
-
-	// if ((is_bg = (*cmd->argv[cmd->argc-1]=='&')) != 0)
-	// 	cmd->argv[--cmd->argc] = NULL;
-	
-	// return is_bg;	
+// 	// // check
+// 	// printf("cmd: %s", temppath);
+// 	// for (int i=0; i<cmd->argc; i++) {
+// 	// 	printf("%d, cmd: %s\n", i, cmd->argv[i]);
+// 	// }
 // }
 
-void translate(char* syscall, char* res) {
-	if (isSyscall(syscall)==1){
-	FILE *cmd=popen(syscall, "r");
-    fgets(res, sizeof(res), cmd);  
-    // printf("inside res: %s\n", res);
-    pclose(cmd);}
+void runCommand(struct command *cmd, int isSyscall) {
+	int rc;
+	if (isSyscall==1) {	
+		printf("%s a system call\n", cmd->argv[0]);
+		pid_t childPid = fork();
+		// printf("\nchildPid: %d\n", childPid);
 
-}
+		if (childPid<0) {
+			printf("fork fails\n");
+		}
 
-void runSystemcommand(struct command *cmd, int rc) {
-	pid_t childPid = fork();
-	// printf("\nchildPid: %d\n", childPid);
+		else if (childPid==0) { // child is here				
+			if (execv(cmd->argv[0], cmd->argv) < 0) {
+				printf("%s: Command not found\n", cmd->argv[0]);
+				exit(0);
+			}			
+		}
+		else { //  parent where the shell continues
+			// if (rc)
+			// 	printf("\nChild in background [%d]\n",childPid);
+			// else
+			// 	wait(&childPid);			
+			printf("\nChild in background [%d]\n",childPid);
+			wait(&childPid);
+		}
+	} else {
+		if (findBuiltin(cmd->argv[0])==1) {
+			printf("%s a builtin cmd\n", cmd->argv[0]);
+			if(strcmp(cmd->argv[0], "exit")==0) {
+				exit(0);
+			} else if(strcmp(cmd->argv[0], "cd")==0) {
+				rc = chdir(cmd->argv[1]);
+				if (rc != 0) 
+					// perror("chdir() to /usr failed");
+					write(STDERR_FILENO, error_message, strlen(error_message)); 
+			} else if(strcmp(cmd->argv[0], "path")==0) {
+				execv(cmd->argv[0], cmd->argv);
 
-	if (childPid<0) {
-		printf("fork fails\n");
-	}
-
-	else if (childPid==0) { // child is here
-		if (execvp(cmd->argv[0], cmd->argv) < 0) {
-			printf("%s: Command not found\n", cmd->argv[0]);
-			exit(0);
+				// overwrite
+				// path[0] = "";
+				pathsize=cmd->argc-1;
+				for (int i=0; i<pathsize; i++) {
+					path[i] = cmd->argv[i+1];
+				}
+				// check
+				printf("path is overwritten to: ");
+				for (int i=0; i<pathsize; i++) {
+					printf("%s, ", path[i]);
+				}
+				printf("\n");
+			}
+		} else {
+			printf("neither system call nor builtin\n");
 		}
 	}
-	else { //  parent where the shell continues
-		// if (rc)
-		// 	printf("\nChild in background [%d]\n",childPid);
-		// else
-		// 	wait(&childPid);
-		
-		printf("\nChild in background [%d]\n",childPid);
-		wait(&childPid);
-	}
-
 }
-// void eval (char *cmdline) {
-// 	int rc;
-// 	struct command cmd;
-// 	printf("evaluating command: %s\n", cmdline);
-// 	rc = parse(cmdline, &cmd);
-
-// 	printf("Found command %s\n", cmd.argv[0]);
-
-// 	if (rc==-1) return;
-// 	if (cmd.argv[0]==NULL) return;
-
-// 	if (cmd.builtin==NULL)
-// 		runSystemcommand(&cmd, rc);
-// 	else
-// 		// runBuiltinCommand(&cmd, rc);
-// 		;
-// }
 
 int main(int argc, char *argv[]) 
 {
 	// char cmdline[MAXBUFR];
 
+	path[0] = "/bin/";
+	pathsize=1;
     while (1) {
 		printf("%s", prompt);
 		char *cmdline;
@@ -182,8 +234,8 @@ int main(int argc, char *argv[])
 		// }
 		struct command cmd;
 		parse(cmdline, &cmd);
-
-		runSystemcommand(&cmd, 1);
+		isSyscall = translate(&cmd);
+		runCommand(&cmd, isSyscall);
 
 		// eval(cmdline); // evaluate and pars
 		// printf("command you type: %s\n", argv[1]);
